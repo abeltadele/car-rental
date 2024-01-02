7317f3dc0ec773430764be2012ea0242bbeb334f
abeltadele <abeltad1221@gmail.com>
2024-01-02 05:45:40 -0300

    day-1

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,11 +4,28 @@ import { IoCarSportSharp } from "react-icons/io5";
 
 const navbar = () => {
   return (
-    <div>
-    
-    <IoCarSportSharp className='carIcon'/>
+   <nav>
+     <div className='logoClass' >
+      <div className='logo'>
+      <IoCarSportSharp className='carIcon'/>
+      <h1 id='h1log'>ABEL<span>TAD</span></h1>
+      </div>
+      <navbar>
+        <ul className='ul1'>
+          <li><a href="#"> Home</a></li>
+          <li><a href="#"> About</a></li>
+          <li><a href="#"> Vehicle Model</a></li>
+          <li><a href="#"> Testemonials</a></li>
+          <li><a href="#"> Contact</a></li>
+        </ul>
+      </navbar>
+      <div className='sign-in'>
+        <button>Sign-in</button>
+        <button>Register</button>
+      </div>
     </div>
 
+   </nav>
   )
 }
 
